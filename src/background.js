4ec257fbb9f605041fe21b6f35dca8171a539a98
Andreas Müller <taori@users.noreply.github.com>
2017-11-18 00:50:29 +0100

    cleanup + basics

--- src/background.js
+++ src/background.js
@@ -1,28 +1,98 @@
-function updateCount(tabId, isOnRemoved) {
-  browser.tabs.query({})
-  .then((tabs) => {
-    let length = tabs.length;
-
-    // onRemoved fires too early and the count is one too many.
-    // see https://bugzilla.mozilla.org/show_bug.cgi?id=1396758
-    if (isOnRemoved && tabId && tabs.map((t) => { return t.id; }).includes(tabId)) {
-      length--;
-    }
-
-    browser.browserAction.setBadgeText({text: length.toString()});
-    if (length > 2) {
-      browser.browserAction.setBadgeBackgroundColor({'color': 'green'});
-    } else {
-      browser.browserAction.setBadgeBackgroundColor({'color': 'red'});
-    }
-  });
+async function getTabInfo(tabId) {
+	return await browser.tabs.get(tabId);
 }
 
+async function saveTabStructureMap(map) {
+	await browser.storage.local.set({ structure: map });
+}
 
-browser.tabs.onRemoved.addListener(
-  (tabId) => { updateCount(tabId, true);
+async function getTabStructureMap() {
+	let structure = await browser.storage.local.get("structure");
+	if (structure) {
+		if (structure.structure)
+			return structure.structure;
+		return structure;
+	}
+	return new {};
+}
+
+async function addTabToStructure(tab) {
+	let structure = await getTabStructureMap();
+	let dest = structure[tab.windowId] || [];
+	dest.push(tab.id);
+	console.log(`Tab ${tab.id} pushed to window ${tab.windowId}.`);
+
+	structure[tab.windowId] = dest;
+
+	await saveTabStructureMap(structure);
+}
+
+async function removeTabFromStructure(tabId) {
+	console.log("removeTabFromStructure called.");
+
+	let structure = await getTabStructureMap();
+	console.log(structure);
+
+	for (let windowId in structure) {
+		let tabs = structure[windowId] || [];
+
+		let index = tabs.indexOf(tabId);
+		if (index >= 0) {
+			tabs.splice(index, 1);
+			console.log(`${tabId} removed.`);
+		}
+
+		structure[windowId] = tabs;
+	}
+
+	await saveTabStructureMap(structure);
+}
+
+async function addTab(tab) {
+
+	await removeTabFromStructure(tab.id);
+	await addTabToStructure(tab);
+	let structure = await getTabStructureMap();
+
+	console.log("end of addTab");
+	console.log(structure);
+}
+
+async function removeTab(tab) {
+	await browser.storage.local.clear();
+}
+
+async function attachTab(tab) {
+}
+
+async function detachTab(tab) {
+}
+
+browser.tabs.onRemoved.addListener((tab) => {
+	removeTab(tab);
 });
-browser.tabs.onCreated.addListener(
-  (tabId) => { updateCount(tabId, false);
+browser.tabs.onCreated.addListener((tab) => {
+	addTab(tab);
 });
-updateCount();
+browser.tabs.onDetached.addListener((tab) => {
+	attachTab(tab);
+});
+browser.tabs.onAttached.addListener((tab) => {
+	detachTab(tab);
+});
+
+
+//omnibox.setDefaultSuggestion()
+//Defines the first suggestion that appears in the drop- down when the user enters the keyword for your extension, followed by a space.
+//
+//	Events
+//Edit
+//
+//omnibox.onInputStarted
+//Fired when a the user focuses the address bar and types your extension's omnibox keyword, followed by a space.
+//omnibox.onInputChanged
+//Fired whenever the user's input changes, after they have focused the address bar and typed your extension's omnibox keyword, followed by a space.
+//	omnibox.onInputEntered
+//Fired when the user accepts one of your extension's suggestions.
+//omnibox.onInputCancelled
+//Fired when the user dismisses the address bar drop- down, after they have focused the address bar and typed your extension's omnibox keyword. 
